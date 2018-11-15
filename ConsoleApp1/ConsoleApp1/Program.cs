f01b85891fbc0ef26bb7c672d830c197dbf6a748
cristian edilberto cogua acosta <negrocristian-2030@hotmail.com>
2018-11-15 01:06:52 -0500

    profe me sale un mano de errores cuando pego el codigo aca

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        /*<!DOCTYPE HTML>
+<html>
+<head>
+    <title> The Best For My Pet</title>
+    <style>
+        .body {
+            background-image: url();
+        background-position: center;
+            background-attachment: fixed;
+    }
+    </style>
+    <link rel = "icon"href="C:\Users\Cristian\Documents\Documentos NetBeansProjects"/>
+    
+
+</head>
+<body>
+    <style>
+        .texto{
+            font-size: 50px;
+            color: orange;
+            text-align: center;
+
+        }
+        .texto2 {
+            background-color: black;
+            margin: 10px 470px;
+            border-style: inset;
+            text-align: center;
+            color: yellow;
+
+
+        }
+        .texto3{
+            color: beige;
+            text-indent: 435px;
+
+        }
+        .texto4 {
+            color: springgreen;
+            text-align: center; 
+        }
+    </style>
+    <p class="texto"><The Best For My Pet ></p>
+
+
+</body>
+</html>
+
+    }
+}
